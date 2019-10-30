9f6e69996cd26053c0a869f476bea9ce6a6a28dd
Dmitrii Usanov <dmitrii.usanov@workato.com>
2019-10-30 21:24:23 +0400

    Added sample project

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const nanoid = require('nanoid');
+require('dotenv').config();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  const token = jwt.sign(
+      {
+        sub: `${process.env.WK_API_KEY}:${process.env.WK_USER_ID}`,
+        jti: nanoid()
+      },
+      process.env.WK_JWT_PRIVATE_KEY,
+      {algorithm: 'RS256'}
+  );
+
+  const host = process.env.WK_HOST;
+
+  res.render('index', { token, host, id: 62 });
+});
+
+module.exports = router;
