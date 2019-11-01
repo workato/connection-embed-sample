d53fd2f7e613e04b075ad0b1c7afd946852c427a
Dmitrii Usanov <dmitrii.usanov@workato.com>
2019-11-01 20:09:31 +0400

    remove logs

--- server/index.js
+++ server/index.js
@@ -33,9 +33,6 @@ export default app => {
             }
         };
 
-        console.dir(options);
-        console.log(`${process.env.VUE_APP_WK_HOST}/api/managed_users/${process.env.WK_USER_ID}/connections`);
-
         https.get(`${process.env.VUE_APP_WK_HOST}/api/managed_users/${process.env.WK_USER_ID}/connections`, options, (res) => {
             if (res.statusCode !== 200) {
                 const error = `Request Failed. Status code: ${res.statusCode}.Message: ${res.statusMessage}`;
